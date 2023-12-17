2f9b2e5a9d0e6d4118b14d804c2ebaa9abc8e367
hassantoon <154087115+hassantoon@users.noreply.github.com>
2023-12-17 22:02:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
+import streamlit as st
+def get_or_create_eventloop():
+    try:
+        return asyncio.get_event_loop()
+    except RuntimeError as ex:
+        if "There is no current event loop in thread" in str(ex):
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            return asyncio.get_event_loop()
+
+loop = get_or_create_eventloop()
+asyncio.set_event_loop(loop)
+
 import telebot
 import telegram
 from telegram import Bot, Update
